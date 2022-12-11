d8d63d1780f824ef38338511931fbe08a8ffae9c
Lap_Javi-Local <javixxmc@gmail.com>
2022-12-11 12:29:42 -0600

    ---|Efectos agregados|--- ---|Pendientes|--- -> Arreglar menú sandwich -> Agregar otras secciones -> Remplazar imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-const toggleMenuElement = document.getElementById('toggle-menu');
-const mainMenuElement = document.getElementById('main-menu');
-
-toggleMenuElement.addEventListener('click', () => {
-  mainMenuElement.classList.toggle('main-menu--show');
-});
+window.addEventListener("scroll", function()
+{
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.pageYOffset > 0);
+}
+)
